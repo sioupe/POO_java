4b851bf4048f9f063aef8738d525d06207f031f9
johan guillen <sioupes34@gmail.com>
2024-10-04 14:02:44 +0200

    POO tp5

---
+++ src/main/java/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,30 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+    private int numCompte;
+    private float soldeCompte;
+
+    public Compte(){
+        this.numCompte=0;
+        this.soldeCompte = 0.0f;
+    }
+    public Compte(int numCompte,float soldeCompte){
+        this.numCompte=numCompte;
+        this.soldeCompte = soldeCompte;
+    }
+
+    public int getNumCompte() {
+        return this.numCompte;
+    }
+    public float getSoldeCompte(){
+        return this.soldeCompte;
+    }
+    public void setNumCompte(int numCompte){
+        this.numCompte=numCompte;
+
+    }
+    public void setSoldeCompte(float soldeCompte){
+        this.soldeCompte=soldeCompte;
+    }
+
+}
---
+++ src/main/java/fr/diginamic/banque/test/TestBanque.java
@@ -0,0 +1,10 @@
+package fr.diginamic.banque.test;
+
+import fr.diginamic.banque.entites.Compte;
+
+public class TestBanque {
+    public static void main(String[] args) {
+        Compte compte1 = new Compte(1,56.3f);
+        System.out.println(compte1);
+    }
+}
