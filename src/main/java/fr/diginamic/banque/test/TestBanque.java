d44bde60a548f372cf8555a12f8dd17a656bbadc
johan guillen <sioupes34@gmail.com>
2024-10-04 14:03:19 +0200

    POO tp5

--- src/main/java/fr/diginamic/banque/test/TestBanque.java
+++ src/main/java/fr/diginamic/banque/test/TestBanque.java
@@ -5,6 +5,6 @@ import fr.diginamic.banque.entites.Compte;
 public class TestBanque {
     public static void main(String[] args) {
         Compte compte1 = new Compte(1,56.3f);
-        System.out.println(compte1);
+        System.out.println(compte1);//ressort un pointeur
     }
 }
