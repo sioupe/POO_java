6730db11d4b5363c7e08174d4bbf0434a513a107
johan guillen <sioupes34@gmail.com>
2024-10-09 15:44:23 +0200

    tp set ex2

--- src/main/java/fr/diginamic/listes/TestVilles.java
+++ src/main/java/fr/diginamic/listes/TestVilles.java
@@ -9,10 +9,9 @@ public class TestVilles {
         Collections.addAll(listVille,new Ville("Nice",343000),new Ville("Carcassonne",47800),new Ville("Narbonne",53400),new Ville("Lyon",484000),new Ville("Foix",9700),new Ville("Pau",77200),new Ville("Marseille",850700),new Ville("Tarbes",40600));
         System.out.println(listVille);
         Ville villePlusPeuple = new Ville(listVille.getFirst());
-
-
-
         Ville villeMoinsPeuple = new Ville(listVille.getFirst());
+
+        
         for (Ville ville :listVille ) {
             if (ville.getNbHab() > villePlusPeuple.getNbHab()) {
                 villePlusPeuple=ville;
