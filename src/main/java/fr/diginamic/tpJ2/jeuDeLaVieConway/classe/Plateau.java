b281cc7219f604cfb59664c134f092da5994a318
johan guillen <sioupes34@gmail.com>
2024-10-09 19:44:44 +0200

    tp auf JeuxDeLaVie methode contourCellule

--- src/main/java/fr/diginamic/tpJ2/jeuDeLaVieConway/classe/Plateau.java
+++ src/main/java/fr/diginamic/tpJ2/jeuDeLaVieConway/classe/Plateau.java
@@ -14,6 +14,19 @@ public class Plateau {
         }
 
     }
+    public ArrayList<Boolean> contourCellule(int x,int y){
+        ArrayList<Boolean> celluleAutour =new ArrayList<Boolean>();
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                if (x == 0 && y==0) {
+                    celluleAutour.add(false);
+                }else {
+                    celluleAutour.add(plateau.get(x+j).get(y+i).getEtat());
+                }
+
+            }
+        }
+    }
 
     @Override
     public String toString() {
