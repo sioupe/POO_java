b057bc9762f5c0d1780cc3230f3b01c7398fc995
johan guillen <sioupes34@gmail.com>
2024-10-09 11:23:18 +0200

    tp liste ex1

---
+++ src/main/java/fr/diginamic/listes/FusionListe.java
@@ -0,0 +1,28 @@
+package fr.diginamic.listes;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class FusionListe {
+    public static void main(String args[]) {
+        List<String> liste1 = new ArrayList<String>();
+        liste1.add("Rouge");
+        liste1.add("Vert");
+        liste1.add("Orange");
+        List<String> liste2 = new ArrayList<String>();
+        liste2.add("Blanc");
+        liste2.add("Bleu");
+        liste2.add("Orange");
+        List<String> liste3 = new ArrayList<String>();
+
+
+        for (String couleur :liste1 ) {
+            liste3.add(couleur);
+        }
+        for (String couleur :liste2 ) {
+            liste3.add(couleur);
+        }
+    }
+}
