503a5aaf338b534136532ebbc6b36ba6bd3ceb6e
johan guillen <sioupes34@gmail.com>
2024-10-09 15:34:22 +0200

    tp set ex2

--- src/main/java/fr/diginamic/listes/TestVilles.java
+++ src/main/java/fr/diginamic/listes/TestVilles.java
@@ -9,29 +9,24 @@ public class TestVilles {
         Collections.addAll(listVille,new Ville("Nice",343000),new Ville("Carcassonne",47800),new Ville("Narbonne",53400),new Ville("Lyon",484000),new Ville("Foix",9700),new Ville("Pau",77200),new Ville("Marseille",850700),new Ville("Tarbes",40600));
         System.out.println(listVille);
         Ville villePlusPeuple = new Ville(listVille.getFirst());
+
+
+
+        Ville villeMoinsPeuple = new Ville(listVille.getFirst());
         for (Ville ville :listVille ) {
             if (ville.getNbHab() > villePlusPeuple.getNbHab()) {
                 villePlusPeuple=ville;
             }
-        }
-
-        System.out.println("ville la plus peuplé = "+villePlusPeuple);
-
-        Ville villeMoinsPeuple = new Ville(listVille.getFirst());
-        for (Ville ville :listVille ) {
             if (ville.getNbHab() < villeMoinsPeuple.getNbHab()) {
                 villeMoinsPeuple =ville;
             }
-        }
-        listVille.remove(villeMoinsPeuple);
-        System.out.println(listVille);
-
-
-        for (Ville ville :listVille ) {
             if (ville.getNbHab() > 100000) {
                 ville.setNom(ville.getNom().toUpperCase());
             }
         }
+        listVille.remove(villeMoinsPeuple);
+        System.out.println("ville la plus peuplé = "+villePlusPeuple);
+
         System.out.println(listVille);
 
 
