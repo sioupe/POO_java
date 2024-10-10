4c59695b4d8ee8d58b2d87d7bc9c4b19c9a733a0
johan guillen <sioupes34@gmail.com>
2024-10-10 11:11:26 +0200

    tp fichier ex1

--- src/main/java/fr/diginamic/fichier/LectureFichier.java
+++ src/main/java/fr/diginamic/fichier/LectureFichier.java
@@ -1,7 +1,16 @@
 package fr.diginamic.fichier;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class LectureFichier {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("E:\\Diginamic\\approche-objet\\recensement.csv");
+        List<String> lines = Files.readAllLines(path);
+        System.out.println(lines);
     }
 }
