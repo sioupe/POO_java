d1384f8011a8b45fef97e910f51c422b8f2d9ec3
johan guillen <sioupes34@gmail.com>
2024-10-10 16:52:18 +0200

    tp map

---
+++ src/main/java/fr/diginamic/maps/CreationEtManipulationMap.java
@@ -0,0 +1,30 @@
+package fr.diginamic.maps;
+
+import java.security.Key;
+import java.util.HashMap;
+
+public class CreationEtManipulationMap {
+    public static void main(String args[]) {
+        HashMap<Integer, String> mapVilles = new HashMap<>();
+        mapVilles.put(13, "Marseille");
+        mapVilles.put(34, "Montpellier");
+        mapVilles.put(44, "Nantes");
+        mapVilles.put(75, "Paris");
+        mapVilles.put(31, "Toulouse");
+        mapVilles.put(59, "Lille");
+        mapVilles.put(69, "Lyon");
+        mapVilles.put(33, "Bordeaux");
+
+        for (Integer key :mapVilles.keySet() ) {
+            System.out.println(key+ "|" +mapVilles.get(key ));
+        }
+        for (String value : mapVilles.values()) {
+            System.out.println(value);
+        }
+        System.out.println(mapVilles.size());
+
+
+    }
+}
+
+
